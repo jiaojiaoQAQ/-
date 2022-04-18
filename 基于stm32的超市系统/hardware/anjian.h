d988f79d74c5e9e8260a62fe6b77e8a1864143f0
皎皎 <3412485034@qq.com>
2022-04-18 13:01:34 +0000

    删除文件 基于stm32的超市系统

--- 基于stm32的超市系统/hardware/anjian.h
+++
@@ -1,6 +0,0 @@
-#ifndef __ANJIAN_H
-#define __ANJIAN_H
-#include "sys.h"
-void anjian_init(void);
-unsigned char key(void);
-#endif
--- 基于stm32的超市系统/hardware/duqu.h
+++
@@ -1,8 +0,0 @@
-#ifndef __DUQU_H
-#define __DUQU_H
-#include "sys.h"
-void duqu(void);
-void duqu_init(void);
-void duqu_zimu(void);
-#endif
-
